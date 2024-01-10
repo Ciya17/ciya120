41818882fd79ef9b0f7cf217bfcec264561a4e42
ciya silva <ciyasilva1278@gmail.com>
2024-01-10 11:18:46 +0530

    commit to ciya17

---
+++ src/rbca20/Lciya120.java
@@ -0,0 +1,14 @@
+package rbca20;
+
+public class Lciya120 {
+
+	public Lciya120() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
