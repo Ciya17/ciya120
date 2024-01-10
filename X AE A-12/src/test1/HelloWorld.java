7f7a722767a4f582ae4a9122cbbdb897b7d798ea
samuel demelo <samueldemelo0832@gmail.com>
2024-01-10 09:55:50 +0530

    commitCiya

---
+++ X AE A-12/src/test1/HelloWorld.java
@@ -0,0 +1,20 @@
+package test1;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+	void SecondBranch() {
+		System.out.println("Coding is fun");
+		System.out.println("Java");
+		
+	}
+
+}
