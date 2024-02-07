091924fe57260ea8176fdcb49675a5a9a9f92af2
Ciya Silva <ciyasilva1278@gmail.com>
2024-02-07 10:33:30 +0530

    Commit Exam

---
+++ GitHub120/src/JavaExam120/Dog.java
@@ -0,0 +1,17 @@
+package JavaExam120;
+
+public class Dog {
+	private String name;
+	private String breed;
+	public Dog(String name,String breed)
+	{
+		this.name=name;
+		this.breed=breed;
+	}
+	public String getBreed() {
+		return breed;
+	}
+
+	}
+
+
